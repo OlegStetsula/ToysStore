4eec98d01e90a3cb4d8e5934ba059c5f33ef7ecc
OlegStetsula <olegstetsula@gmail.com>
2018-12-21 20:47:23 +0200

    Creating data model

--- ToysStore/Global.asax.cs
+++ ToysStore/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Data.Entity;
+using ToysStore.Models;
 
 namespace ToysStore
 {
@@ -16,6 +18,8 @@ namespace ToysStore
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer(new ProductDatabaseInitializer());
         }
     }
 }
